3ccf60f2f0eec5865146e38859dabf6a8303f6b2
LikeToProgramm <liketoprogram@outlook.com>
2017-04-24 00:31:05 +0300

    Added header for string tokinization.

---
+++ Lexer.h
@@ -0,0 +1,51 @@
+#pragma once
+#include "Token.h"
+#include <vector>
+
+using namespace std;
+
+//This class let you parse string to vector of tokens
+class Lexer {
+private:
+	const int size;
+	const string input;
+	const string OPERATOR_CHARS = "+-*/";
+	const TokenType OPERATOR_TOKENS[4] = {
+		PLUS, MINUS,
+		STAR, SLASH
+	};
+	vector<Token> result;
+	int pos = 0;
+
+	//Register tokens with data
+	void addToken(TokenType type, string value) {
+		Token t(type, value);
+		result.push_back(t);
+	}
+
+	//Register tokens without data (Like operators)
+	void addToken(TokenType type) {
+		addToken(type, "");
+	}
+
+	//This method returns a char at some position
+	char get(int relativePosition) {
+		int position = pos + relativePosition;
+		if (position >= size) return '\0';
+		return input[position];
+	}
+
+	//This method returns a char at next position
+	char next() {
+		pos++;
+		return get(0);
+	}
+
+public:
+	Lexer(string input) : input(input), size(input.size()) {}
+
+	vector<Token> tokenize() {
+
+		return result;
+	}
+};
